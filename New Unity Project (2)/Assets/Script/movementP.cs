5d90253afdc681865048355a2f4971a7d27564b9
xthunder81 <37144224+xthunder81@users.noreply.github.com>
2018-03-16 13:14:24 +0700

    penambahan movement player

--- New Unity Project (2)/Assets/Script/movementP.cs
+++ New Unity Project (2)/Assets/Script/movementP.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class movementP : MonoBehaviour {
 
+	public float speed;
+	public float jump;
+	Rigidbody rb ;
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += Vector3.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position += Vector3.back * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			transform.position += Vector3.up * speed * jump * Time.deltaTime;
+		}
+
 	}
 }
